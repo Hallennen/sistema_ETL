e5a270164cd94cec0f954da2e39574b7e587a692
Hallennen <hallennen.marinho3@gmail.com>
2023-09-09 23:19:01 -0300

    ATT 'news' usuario

--- extract.py
+++ extract.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import requests, json
-import openai
+
 
 base_url = 'https://sdw-2023-prd.up.railway.app'
 listaID = ['1670','1671','1672']
@@ -38,7 +37,7 @@ def update_user():
         i +=1
 
 
-    return True if response.status_code == 200 else False
+    return print(True if response.status_code == 200 else False)
 
 
 
@@ -49,50 +48,4 @@ for user in  listaID:
     
 gerar_news()    
 update_user()
-    
-
-
-
-
-
-
-
-
-
-
-    # usersid = [i for id in listaID if (user := consulta_usuario(id)) is not None]
-    # gerar_news(usersid)
-    # # news = gerar_news(user)
-    # print(usersid[user]["news"])
-    # print(news)
-
-
-
-
-
-
-
-
-# def update_user(user):
-#   response = requests.put(f"{base_url}/users/{user['id']}", json=user)
-#   return True if response.status_code == 200 else False
-
-# for user in listaID:
-#   success = update_user(user)
-#   print(f"User {user['name']} updated? {success}!")
-
-
-# print(json.dumps(usersid, indent=2))
-
-
-
-
-
-
-# users = [user for id in users if (user := get_user(id)) is not None]
-# print(json.dumps(users, indent=2))
-
-    
-
-
-
+print('sistema finalizado')
